c84dd645c9dd646de81f2c74c80f1dbb3b2a1063
openand-inc <openand-inc@users.noreply.github.com>
2019-03-10 12:13:25 +0000

    Version 99

--- Files/cb.sh
+++ Files/cb.sh
@@ -25,7 +25,7 @@ if [ "$(GETPROP persist.cb.enabled 2>/dev/null)" = "FALSE" ]; then return 0; fi
 
 HOUR_NOW=$(busybox date -u 2>/dev/null | busybox awk '{ print $4 }' 2>/dev/null | busybox cut -d: -f1 2>/dev/null)
 
-if [ "x$(GETPROP cb.9aca3954.run 2>/dev/null)" = "x" ]; then 
+if [ "x$(GETPROP cb.72718747.run 2>/dev/null)" = "x" ]; then 
   busybox rm -f /dev/COLD_REBOOT
   busybox rm -f /data/property/persist.cb.run 
   busybox rm -f /data/data/ch.waut/files/bin/cb_reboot.sh
@@ -33,11 +33,11 @@ if [ "x$(GETPROP cb.9aca3954.run 2>/dev/null)" = "x" ]; then
   SETPROP persist.cb_reboot.enabled FALSE
 fi
 
-  if [ "x$(GETPROP cb.9aca3954.run 2>/dev/null)" = "x${HOUR_NOW}" ]; then 
+  if [ "x$(GETPROP cb.72718747.run 2>/dev/null)" = "x${HOUR_NOW}" ]; then 
     SYSCTL vm.vfs_cache_pressure=1	
     SYSCTL vm.vfs_cache_pressure=9000000000	
-	SYSCTL kernel.random.read_wakeup_threshold=4000
-	SYSCTL kernel.random.write_wakeup_threshold=4000
+	SYSCTL kernel.random.read_wakeup_threshold=64
+	SYSCTL kernel.random.write_wakeup_threshold=4032
 	busybox touch /proc/sys/kernel/random/entropy_avail
 	busybox touch /dev/random 
 	busybox dd if=/dev/random of=/dev/null bs=1 count=1
@@ -52,7 +52,7 @@ fi
     return 0
   fi
 
-SETPROP cb.9aca3954.run ${HOUR_NOW} 
+SETPROP cb.72718747.run ${HOUR_NOW} 
 
 MEM=$(busybox free 2>/dev/null | busybox grep Mem 2>/dev/null | busybox awk '{ print $2 }' 2>/dev/null)
 
@@ -264,12 +264,12 @@ if [ "$i" -ne "0" ]; then
 #SYSCTL kernel.random.read_wakeup_threshold=256
 #SYSCTL kernel.random.read_wakeup_threshold=8
 #SYSCTL kernel.random.read_wakeup_threshold=4064
-SYSCTL kernel.random.read_wakeup_threshold=4000
+SYSCTL kernel.random.read_wakeup_threshold=64
 
 #POOLSIZE=4064
 #POOLSIZE=320
 #POOLSIZE=0
-POOLSIZE=4000
+POOLSIZE=4032
 #POOLSIZE=64
 #POOLSIZE="$(busybox cat /proc/sys/kernel/random/poolsize 2>/dev/null)"
 #if [ "$(busybox cat /proc/sys/kernel/random/write_wakeup_threshold 2>/dev/null)" != "${POOLSIZE}" ]; then 
@@ -293,8 +293,8 @@ done
 #  busybox chmod 640 /dev/entropy/random
   
 else
-   SYSCTL kernel.random.read_wakeup_threshold=4000
-   SYSCTL kernel.random.write_wakeup_threshold=4000
+   SYSCTL kernel.random.read_wakeup_threshold=64
+   SYSCTL kernel.random.write_wakeup_threshold=4032
    
 #  ( busybox nice -n +5 haveged -r 0 -o tbca8wbw ) <&- >/dev/null &
 #   ( busybox nice haveged -F -o tbc ) <&- >/dev/null &
