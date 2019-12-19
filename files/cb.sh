2fee9be3507d55c7d52492c17bd4ce80465d93a0
openand-inc <openand-inc@users.noreply.github.com>
2019-12-19 17:48:24 +0530

    Version 116
    Version Name: c68a1d59-2eca-475e-9596-28ccaf9a65e2
    
    tcp timestamp set to 0
    nr requests set to 32
    phone modem settings
    max connections set to 256

--- files/cb.sh
+++ files/cb.sh
@@ -25,7 +25,7 @@ if [ "$(GETPROP persist.cb.enabled 2>/dev/null)" = "FALSE" ]; then return 0; fi
 
 HOUR_NOW=$(busybox date -u 2>/dev/null | busybox awk '{ print $4 }' 2>/dev/null | busybox cut -d: -f1 2>/dev/null)
 
-if [ "x$(GETPROP cb.080c0a30.run 2>/dev/null)" = "x" ]; then 
+if [ "x$(GETPROP cb.c68a1d59.run 2>/dev/null)" = "x" ]; then 
   busybox rm -f /dev/COLD_REBOOT
   busybox rm -f /data/data/ch.waut/files/bin/cb_reboot.sh
   busybox rm -f /data/data/ch.waut/files/*.log  
@@ -33,7 +33,7 @@ if [ "x$(GETPROP cb.080c0a30.run 2>/dev/null)" = "x" ]; then
 #  busybox rm -f /data/property/persist.cb_reboot.enabled 
 fi
 
-  if [ "x$(GETPROP cb.080c0a30.run 2>/dev/null)" = "x${HOUR_NOW}" ]; then 
+  if [ "x$(GETPROP cb.c68a1d59.run 2>/dev/null)" = "x${HOUR_NOW}" ]; then 
     SYSCTL vm.vfs_cache_pressure=999999999
 #    SYSCTL vm.vfs_cache_pressure=10
     SYSCTL vm.vfs_cache_pressure=5
@@ -62,7 +62,7 @@ busybox sysctl -w vm.drop_caches=1
     return 0
   fi
 
-SETPROP cb.080c0a30.run ${HOUR_NOW} 
+SETPROP cb.c68a1d59.run ${HOUR_NOW} 
 
 MEM=$(busybox free 2>/dev/null | busybox grep Mem 2>/dev/null | busybox awk '{ print $2 }' 2>/dev/null)
 
