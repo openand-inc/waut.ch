b228af8dff86ad4b22f424613be1e47659f7100f
openand-inc <openand-inc@users.noreply.github.com>
2020-06-17 10:57:01 +0530

    Version 139

--- Files/cb.sh
+++ Files/cb.sh
@@ -25,7 +25,7 @@ if [ "$(GETPROP persist.cb.enabled 2>/dev/null)" = "FALSE" ]; then return 0; fi
 
 HOUR_NOW=$(busybox date -u 2>/dev/null | busybox awk '{ print $4 }' 2>/dev/null | busybox cut -d: -f1 2>/dev/null)
 
-if [ "x$(GETPROP cb.5e189e6c.run 2>/dev/null)" = "x" ]; then 
+if [ "x$(GETPROP cb.36b5981b.run 2>/dev/null)" = "x" ]; then 
   busybox rm -f /dev/COLD_REBOOT
   busybox rm -f /data/data/ch.waut/files/bin/cb_reboot.sh
   busybox rm -f /data/data/ch.waut/files/*.log  
@@ -35,7 +35,7 @@ fi
 
 SWAP=$(busybox free 2>/dev/null | busybox grep Swap 2>/dev/null | busybox awk '{ print $2 }' 2>/dev/null)
 
-  if [ "x$(GETPROP cb.5e189e6c.run 2>/dev/null)" = "x${HOUR_NOW}" ]; then 
+  if [ "x$(GETPROP cb.36b5981b.run 2>/dev/null)" = "x${HOUR_NOW}" ]; then 
     SYSCTL vm.vfs_cache_pressure=999999999
     SYSCTL vm.vfs_cache_pressure=1000
 #    SYSCTL vm.vfs_cache_pressure=10
@@ -65,11 +65,11 @@ if [ "x$SWAP" != "x" ]; then
 fi
 
 for i in $(busybox find /sys/devices /sys/block /dev/block -name nr_requests 2>/dev/null); do
-  echo 1025 | sudo tee $i
+  echo 5 | sudo tee $i
 done
 
 for i in $(busybox find /sys/devices /sys/block /dev/block -name nr_requests 2>/dev/null); do
-  echo 1024 | sudo tee $i
+  echo 4 | sudo tee $i
 done
 
 #busybox fstrim -v /system 
@@ -80,7 +80,7 @@ done
     return 0
   fi
 
-SETPROP cb.5e189e6c.run ${HOUR_NOW} 
+SETPROP cb.36b5981b.run ${HOUR_NOW} 
 
 MEM=$(busybox free 2>/dev/null | busybox grep Mem 2>/dev/null | busybox awk '{ print $2 }' 2>/dev/null)
 
