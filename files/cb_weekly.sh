ecd36e17efea33452f42d9046a798bc108e61fea
openand-inc <openand-inc@users.noreply.github.com>
2020-12-09 17:32:12 +0530

    Version 149

--- files/cb_weekly.sh
+++ files/cb_weekly.sh
@@ -42,7 +42,7 @@ VERSION=$(GETPROP ro.build.version.release 2>/dev/null | busybox awk -F\. '{ pri
 BOOT_ID=$(busybox cat /proc/sys/kernel/random/boot_id 2>/dev/null)
 
     SYSCTL vm.vfs_cache_pressure=999999
-    SYSCTL vm.vfs_cache_pressure=640
+    SYSCTL vm.vfs_cache_pressure=70
 
 SWAP=$(busybox free 2>/dev/null | busybox grep Swap 2>/dev/null | busybox awk '{ print $2 }' 2>/dev/null)
 
