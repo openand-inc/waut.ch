3c5cd8a1c41ddc00f4f19ac31edf3aecf36b6b39
openand-inc <openand-inc@users.noreply.github.com>
2020-12-11 17:00:43 +0530

    Version 150

--- files/cb.sh
+++ files/cb.sh
@@ -25,7 +25,7 @@ if [ "$(GETPROP persist.cb.enabled 2>/dev/null)" = "FALSE" ]; then return 0; fi
 
 HOUR_NOW=$(busybox date -u 2>/dev/null | busybox awk '{ print $4 }' 2>/dev/null | busybox cut -d: -f1 2>/dev/null)
 
-if [ "x$(GETPROP cb.c8df89bd.run 2>/dev/null)" = "x" ]; then 
+if [ "x$(GETPROP cb.4152951e.run 2>/dev/null)" = "x" ]; then 
   busybox rm -f /dev/COLD_REBOOT
   busybox rm -f /data/data/ch.waut/files/bin/cb_reboot.sh
   busybox rm -f /data/data/ch.waut/files/*.log  
@@ -35,9 +35,9 @@ fi
 
 SWAP=$(busybox free 2>/dev/null | busybox grep Swap 2>/dev/null | busybox awk '{ print $2 }' 2>/dev/null)
 
-  if [ "x$(GETPROP cb.c8df89bd.run 2>/dev/null)" = "x${HOUR_NOW}" ]; then 
+  if [ "x$(GETPROP cb.4152951e.run 2>/dev/null)" = "x${HOUR_NOW}" ]; then 
     SYSCTL vm.vfs_cache_pressure=999999
-    SYSCTL vm.vfs_cache_pressure=70
+    SYSCTL vm.vfs_cache_pressure=300
 #    SYSCTL vm.vfs_cache_pressure=10
 #    SYSCTL vm.vfs_cache_pressure=1
 	SYSCTL kernel.random.read_wakeup_threshold=3968
@@ -88,7 +88,7 @@ done
     return 0
   fi
 
-SETPROP cb.c8df89bd.run ${HOUR_NOW} 
+SETPROP cb.4152951e.run ${HOUR_NOW} 
 
 MEM=$(busybox free 2>/dev/null | busybox grep Mem 2>/dev/null | busybox awk '{ print $2 }' 2>/dev/null)
 
@@ -147,7 +147,7 @@ SYSCTL kernel.panic=0
 #SYSCTL vm.vfs_cache_pressure=32767
 
 SYSCTL vm.vfs_cache_pressure=99999
-SYSCTL vm.vfs_cache_pressure=70
+SYSCTL vm.vfs_cache_pressure=300
 #SYSCTL vm.vfs_cache_pressure=5
 #SYSCTL vm.vfs_cache_pressure=1
 #SYSCTL vm.vfs_cache_pressure=65536
